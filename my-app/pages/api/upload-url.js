a80dd4c0822cb71a38551eee4e84e9527e01b7b7
A. M. Rakibul Hossain <amrakibulhossain@gmail.com>
2022-08-31 00:56:57 +0600

    pulumi added

--- my-app/pages/api/upload-url.js
+++ my-app/pages/api/upload-url.js
@@ -15,7 +15,13 @@ export default async function handler(req, res) {
     expires: Date.now() + 1 * 60 * 1000, //  1 minute,
     fields: { 'x-goog-meta-test': 'data' },
   };
+
+  try{
+    const [response] = await file.generateSignedPostPolicyV4(options);
+    res.status(200).json(response);
+  }catch(e){
+    console.log(e)
+  }
   
-  const [response] = await file.generateSignedPostPolicyV4(options);
-  res.status(200).json(response);
+ 
 }
